68946f3302d7163cf571898e1b4eacaa1a179510
Melon <chydmelon@gmail.com>
2024-08-12 20:20:17 +0800

    new config

--- contracts/DuckNft.sol
+++ contracts/DuckNft.sol
@@ -10,7 +10,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyDuck is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
     uint256 MAX_SUPPLY = 100;
-    event nftTransferEvent(address _from, address _to, uint _tokenId);
+    event nftTransferEvent(
+        address _from,
+        address _to,
+        uint _tokenId,
+        uint price
+    );
     event mintEvent(address to, uint tokenId, string uri);
 
     constructor(
@@ -27,7 +32,6 @@ contract MyDuck is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     }
 
     // The following functions are overrides required by Solidity.
-
     function _update(
         address to,
         uint256 tokenId,
@@ -63,18 +67,34 @@ contract MyDuck is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     //监听transfer的方法
     function nftTransfer(address _from, address _to, uint _tokenId) public {
         _safeTransfer(msg.sender, _to, _tokenId);
-        emit nftTransferEvent(_from, _to, _tokenId);
+        emit nftTransferEvent(_from, _to, _tokenId, 0); // 这里的0是区分直接转移，而不是buy的
     }
 
-    //此方法已经转到数据库做处理，暂时是不需要了
-    // function walletOfOwner(
-    //     address owner
-    // ) public view returns (uint256[] memory) {
-    //     uint256 ownerTokenCount = balanceOf(owner);
-    //     uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-    //     for (uint256 i; i < ownerTokenCount; i++) {
-    //         tokenIds[i] = tokenOfOwnerByIndex(owner, i);
-    //     }
-    //     return tokenIds;
-    // }
+    // 用于购买已经铸造的NFT
+    function buy(uint256 tokenId) public payable {
+        address owner = ownerOf(tokenId);
+        require(owner != address(0), "Token does not exist");
+        require(msg.sender != owner, "You cannot buy your own token");
+        require(msg.value > 0, "ETH value must be greater than zero");
+
+        // 将NFT从当前拥有者转移到购买者
+        _safeTransfer(owner, msg.sender, tokenId, "");
+
+        // 将ETH转移给卖家
+        payable(owner).transfer(msg.value);
+
+        emit nftTransferEvent(owner, msg.sender, tokenId, msg.value);
+    }
 }
+
+//此方法已经转到数据库做处理，暂时是不需要了
+// function walletOfOwner(
+//     address owner
+// ) public view returns (uint256[] memory) {
+//     uint256 ownerTokenCount = balanceOf(owner);
+//     uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+//     for (uint256 i; i < ownerTokenCount; i++) {
+//         tokenIds[i] = tokenOfOwnerByIndex(owner, i);
+//     }
+//     return tokenIds;
+// }
