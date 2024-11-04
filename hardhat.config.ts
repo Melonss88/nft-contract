4d6f361f807e9f3e8287b6edeef521b29ab100d3
Melon <chiyudong@hotbit.pro>
2024-11-04 22:57:42 +0800

    add sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,50 @@
+// import { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+// import "@nomiclabs/hardhat-ethers"
+// import '@openzeppelin/hardhat-upgrades'
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const PRIVATE_KEY = '49835bf8f31b10300f647bba28d023554e4a06b98dae3267c44bd7ecdf18e520'
+// const ETHERSCAN_API_KEY = 'H466KP9R8EC91D9CBFQ5H18F3J3CI952YU'
+
+// const config: HardhatUserConfig = {
+//   solidity: "0.8.24",
+//   networks: {
+//     sepolia: {
+//       url: "https://eth-sepolia.g.alchemy.com/v2/vD4A4zO3XV2O6VQjvWa0H1UZa7ZRu1sx", 
+//       accounts: [`0x${PRIVATE_KEY}`]  
+//     }
+//   },
+//   etherscan: {
+//     apiKey: ETHERSCAN_API_KEY
+//   }
+// };
+
+
+// export default config;
+
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-ethers"
-import '@openzeppelin/hardhat-upgrades'
+import "@nomiclabs/hardhat-ethers";
 import dotenv from 'dotenv';
 dotenv.config();
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '49835bf8f31b10300f647bba28d023554e4a06b98dae3267c44bd7ecdf18e520'; // 使用环境变量
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'H466KP9R8EC91D9CBFQ5H18F3J3CI952YU'; // 使用环境变量
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/vD4A4zO3XV2O6VQjvWa0H1UZa7ZRu1sx", 
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  // etherscan: {
+  //   apiKey: ETHERSCAN_API_KEY,
+  // },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,24 @@
-import "hardhat/console.sol";
-import { Web3 } from 'web3'
+// import "hardhat/console.sol";
+// import { Web3 } from 'web3'
+
+// async function main() {
+//   const web3 = new Web3("http://127.0.0.1:8545/");
+// }
+
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
+// import { ethers } from "hardhat";
+import hre from "hardhat";
 
 async function main() {
-  const web3 = new Web3("http://127.0.0.1:8545/");
+  // const MyNFT = await ethers.getContractFactory("MyDuck");
+  const MyNFT = await hre.ethers.getContractFactory("MyDuck");
+  const myNFT = await MyNFT.deploy("0x463FCfA75aD1c920e0327379812db196c9Db7317");
+  await myNFT.deployed();
+  console.log("MyNFT deployed to:", myNFT.address);
 }
 
 main().catch((error) => {
